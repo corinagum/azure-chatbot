41ab59fbe4f760afde55db0f5858d7e1542f4de7
Kabir Khan <kabirkhan14@hotmail.com>
2016-07-26 18:15:38 -0700

    Added base d3 style and js to display nodes. Setup connection to Test Azure Database for seed data.

--- webinterface/server.js
+++ webinterface/server.js
@@ -1,11 +1,34 @@
-var express = require("express"); 
+var express = require("express");
 var app = express(); 
 var bodyParser = require("body-parser"); 
 var path = require("path"); 
+
 app.use(bodyParser.json()); 
-app.use(express.static(path.join(__dirname,  "./static"))); 
+app.use(express.static(path.join(__dirname,  "./client"))); 
 var route_setter = require("./server/config/routes.js"); 
 route_setter(app); 
 app.listen(8000, function(){ 
  	console.log("running on port 8000"); 
 }); 
+
+var sql = require("seriate")
+
+var connection = {
+    user: "kabirkhan",
+    password: "azurechatbot123!",
+    host: "azurechatbot.database.windows.net",
+    database: "Test",
+	options: {
+        encrypt: true // Use this if you're on Windows Azure 
+    }
+};
+ 
+sql.execute( connection, {
+	name: "selectFromNode",
+	preparedSql: "select * from node",
+} ).then( function( data ) {
+	console.log("Got It!");
+	console.log(data);
+}, function( err ) {
+	console.log( err );
+} );
